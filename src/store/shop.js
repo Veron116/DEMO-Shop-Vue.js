8f2958a758df8b853cc7ed8fea23099da7f8ccae
Nikushka <veronica.ryzhkova@gmail.com>
2021-01-28 17:12:13 +0700

    added render array of items on main page shop

--- src/store/shop.js
+++ src/store/shop.js
@@ -7,6 +7,24 @@ export default {
                descr: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
                 img: require('../assets/img/1.png'),
            },
+           {
+                id: 2,
+                title: 'Nike Default',
+                descr: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+                img: require('../assets/img/4.png'),
+            },
+            {
+                id: 3,
+                title: 'Nike Green',
+                descr: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+                 img: require('../assets/img/7.png'),
+            },
+            {
+                id: 4,
+                title: 'Nike Street',
+                descr: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+                 img: require('../assets/img/10.png'),
+            },
        ] 
     },
     mutations: {},
