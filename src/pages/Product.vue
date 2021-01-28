2320e8b01eecd6b81048e45b03c74009bdd4d8a8
Nikushka <veronica.ryzhkova@gmail.com>
2021-01-28 18:26:30 +0700

    added slider to product card

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -2,9 +2,27 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <img :src="product.img" :alt="product.title"/>
-        <h1>{{product.title}}</h1>
-        <p>{{product.descr}}</p>
+          <div class="product__wrapper">
+              <!-- slider -->
+            <div class="product-slider">
+                <carousel 
+                :perPage="1" 
+                :paginationEnable="true" 
+                paginationColor="#b3b3b3" 
+                paginationActiveColor="#494ce8">
+                    <slide v-for="(slide, index) in product.gallery" :key="index">
+                        <img :src="slide.img" :alt="slide.name"/>
+                    </slide>
+                </carousel>
+            </div>
+
+            <!-- content -->
+            <div class="product-content">
+                <h1 class="title">{{product.title}}</h1>
+                <p>{{product.descr}}</p>
+            </div>
+          </div>
+        
       </div>
     </section>
   </div>
@@ -18,10 +36,22 @@ export default {
         }
     },
     created() {
-        // console.log(this.$route.params.id)
         let id = this.$route.params.id;
         this.product = this.$store.getters.getProduct(id);
         console.log(this.product)
     }
 }
 </script>
+
+<style lang="scss">
+.product__wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.product-slider,
+.product-content {
+    max-width: 48%;
+    text-align: center;
+}
+</style>
